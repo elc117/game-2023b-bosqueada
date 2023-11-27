654021bbcdfd0aa513d19d114e5d023453cd8047
Alan Jacobs <ahjacobs@inf.ufsm.br>
2023-11-27 10:59:15 -0300

    implementando o Fofao

--- Bosqueada/core/src/com/bosqueada/game/Bosqueada.java
+++ Bosqueada/core/src/com/bosqueada/game/Bosqueada.java
@@ -1,15 +1,20 @@
 package com.bosqueada.game;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Bosqueada extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture chao;
+	Texture jacare_testura;
+	Sprite jacare_fofao;
+	float posX, posY;
 
 	// Defina a cor azul bebê (um tom suave de azul)
 	Color babyBlue = new Color(0.678f, 0.847f, 0.902f, 1f);
@@ -18,8 +23,18 @@ public class Bosqueada extends ApplicationAdapter {
 	public void create () {
 
 		batch = new SpriteBatch();
+
+		// textura do personagem
+		jacare_testura = new Texture("jacarefofao.png");
+
+		// textura das arvores de fundo
 		chao = new Texture("chao_floresta_recortado.png");
 
+		// criando o jacare fofao com a textura
+		jacare_fofao = new Sprite(jacare_testura);
+
+		// definindo a posicao inicial
+		jacare_fofao.setPosition(0, 0);
 
 	}
 
@@ -27,9 +42,20 @@ public class Bosqueada extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(babyBlue);
 
+		// Movimenta a sprite do personagem para a esquerda quando a tecla seta esquerda é pressionada
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            jacare_fofao.setX(jacare_fofao.getX() - 20);
+        }
+        // Movimenta a sprite do personagem para a direita quando a tecla seta direita é pressionada
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            jacare_fofao.setX(jacare_fofao.getX() + 20);
+        }
+
 		batch.begin();
 
 		batch.draw(chao, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		jacare_fofao.draw(batch);
 
 		batch.end();
 	}
@@ -37,8 +63,8 @@ public class Bosqueada extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		//cenario1.dispose();
 		chao.dispose();
+		jacare_fofao.getTexture().dispose();
 	}
 
 
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
    Adicionar perguntas no cenário, caso elas sejam respondidas corretamente o jogador recebe moedas e, com as moedas recebidas, ele poderá ir para o proximo cenário, onde terá que enfrentar novas perguntas.
 
 ### Mudamos de Ideia
-   Nossa antiga proposta criava um jogo chato, agora, iremos adicionar o personagem da Bosqueada, o Jacaré Apelaum, ele terá que desviar de meteoritos caidos do céu e capturar as perguntas. Para ganhar é simples, sobreviva;
+   Nossa antiga proposta criava um jogo chato, agora, iremos adicionar o personagem da Bosqueada, o Jacaré Fofão, ele terá que desviar de meteoritos caidos do céu e capturar as perguntas. Para ganhar é simples, sobreviva;
 
 ### Mudança do Cenário
 
